44c49794ffbeb16761cc4c582e07b7b8973352ca
SilasEduardo <silasandrade94@gmail.com>
2022-05-17 15:08:22 -0300

    aula-06

---
+++ aula-06/index.js
@@ -0,0 +1,12 @@
+let umaString = "um texto";
+
+
+console.log(umaString.charAt(2));
+console.log(umaString.indexOf('texto'));
+console.log(umaString.lastIndexOf('o', 10));
+console.log(umaString.match(/[a-z]/g));
+console.log(umaString.search(/x/));
+console.log(umaString.replace('um', 'dois'));
+console.log(umaString.length);
+console.log(umaString.slice(3, 8));
+console.log(umaString.slice(5));
