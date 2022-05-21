0501045c19319f5fa69376ce3d311b0aaae0dcf5
SilasEduardo <silasandrade94@gmail.com>
2022-05-21 16:44:24 -0300

    aula-07

---
+++ aula-07/index.js
@@ -0,0 +1,11 @@
+let num1 = 1;
+let num2 = 2.5;
+
+
+// console.log(num1.toString() + num2)
+// nui = num1.toString(); num1 vira uma string
+// console.log(num1.toString(2)) ver os binarios
+// console.log(num1.toFixed(2)) define as casas decimais
+// console.log(Number.isInteger(num2)) ver se é um numero inteiro
+let temp = num1 * 'ola';
+console.log(Number.isNaN(temp))
