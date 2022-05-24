696a126fa1adb3f187aed31ba4e6dfae51d3bd8f
SilasEduardo <silasandrade94@gmail.com>
2022-05-24 07:25:07 -0300

    aula de math

---
+++ aula-08/index.js
@@ -0,0 +1,10 @@
+ let num1 = 9.811541
+
+
+//  let num2 = Math.floor(num1); Aredonda para baixo
+//  let num3 = Math.ceil(num1); Aredonda para cima
+//  let num4 = Math.round(num1); Aredonda parao numero mais perto
+// console.log(Math.max(1,2,3,5,8,1000)) pega o maior
+// console.log(Math.min(1,2,3,5,8,1000)) pega o menor
+const aliatorio = Math.round( Math.random() * 10 - 2)
+ console.log(aliatorio)
