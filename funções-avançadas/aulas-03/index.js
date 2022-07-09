944cad735fad127bae472c3ed27d17e934fa85ee
SilasEduardo <silasandrade94@gmail.com>
2022-07-09 18:54:31 -0300

    aula

---
+++ funções-avançadas/aulas-03/index.js
@@ -0,0 +1,29 @@
+// let valor = function soma(a, b){
+//     return a + b
+// }
+
+// console.log(valor(10, 2))
+
+
+
+// function criaPessoa(nome, sobrenome, idade){
+//     return{
+//         nome, sobrenome, idade
+//     }
+// }
+
+
+// console.log(criaPessoa('Silas', 'Eduardo', 23))
+
+
+function falaFrase(comeco){
+    function falaResto(resto){
+        return comeco + ' ' + resto;
+    }
+    return falaResto;
+}
+
+const olaMundo = falaFrase('olá')
+
+
+console.log(olaMundo('mundo'))
