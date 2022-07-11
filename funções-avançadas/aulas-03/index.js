08a1794ea8fae3a565778334625ae2f5faee8f45
SilasEduardo <silasandrade94@gmail.com>
2022-07-11 12:07:44 -0300

    aula04

--- funções-avançadas/aulas-03/index.js
+++ funções-avançadas/aulas-03/index.js
@@ -15,13 +15,45 @@
 
 // console.log(criaPessoa('Silas', 'Eduardo', 23))
 
+// function falaFrase(comeco){
+//     function falaResto(resto){
+//         return comeco + ' ' + resto
+//     }
+//     return falaResto
+// }
 
-function falaFrase(comeco){
-    function falaResto(resto){
-        return comeco + ' ' + resto;
+// let fala = falaFrase('ola') /// destro dessa função tenho outra função
+// let resto = fala('mundo') // aqui estou acessando a outra função.
+
+// console.log(resto)
+
+
+// function criaMultiplicador(multiplicador){
+//     return function(n){
+//        return n * multiplicador
+//   }
+
+// }
+
+// let duplica = criaMultiplicador(2)
+// let triplica = criaMultiplicador(3)
+// let quadriplica = criaMultiplicador(4)
+
+// console.log(duplica(2))
+// console.log(triplica(2))
+// console.log(quadriplica(2))
+
+
+function criarTabuada(n){
+    return function (n2){
+        return n * n2
     }
-    return falaResto;
 }
-
-const olaMundo = falaFrase('olá')
-console.log(olaMundo('mundo'))
+for(let i = 1; i <= 10; i++){
+    let criar = criarTabuada(i)
+    console.log(`essa é a tabuada do ${i}`)
+    for(let j = 1; j <= 10; j++){
+        console.log( `${i} x ${j} = ${criar(j)}`)
+    }
+    
+}
