3a343be4b1342c7b4a3f95ae0601cbe4193c3130
SilasEduardo <silasandrade94@gmail.com>
2022-07-16 22:26:30 -0300

    aula-09

--- funções-avançadas/aula-09/assets/js/main.js
+++ funções-avançadas/aula-09/assets/js/main.js
@@ -2,13 +2,45 @@ function criaCalculadora(){
     return{
 
         display: document.querySelector('.display'),
-
-
-
+        btnClear: document.querySelector('.btn-clear'),
 
         inicia(){
             this.clickBotoes();
+            this.precionaEnter();
         },
+        precionaEnter(){
+            document.addEventListener('keyup', (e)=>{
+                if(e.key === 'Enter'){
+                    this.realizaConta()
+                }
+            })
+        },
+
+        crearDisplay() {
+            this.display.value = ''
+        },
+
+        apagaUm() {
+            this.display.value = this.display.value.slice(0, -1);
+        },
+
+        realizaConta() {
+            let conta = this.display.value;
+
+            try{
+                conta = eval(conta)
+                if(!conta){
+                    alert('Conta Invalida');
+                    return
+                }
+                this.display.value = conta;
+            }catch(e){
+                alert('Conta Invalida')
+                console.log(e)
+            }
+        },
+
+
 
         clickBotoes() {
             document.addEventListener('click', (e)=>{
@@ -17,13 +49,26 @@ function criaCalculadora(){
 
                 if(el.classList.contains('btn-num')){
                     this.btnParaDisplay(el.innerText);
-                }
+                };
+
+                if(el.classList.contains('btn-clear')){
+                    this.crearDisplay()
+                };
+
+                if(el.classList.contains('btn-del')) {
+                    this.apagaUm();
+                };
+
+                if(el.classList.contains('btn-eq')){
+                    this.realizaConta();
+                };
 
             });
         },
         btnParaDisplay(value){
-            console.log(value)
-        }
+            this.display.value += value;
+            
+        },
     };
 }
 const calculadora = criaCalculadora()
