c68a91ea7c64d7e855eba710d95cac6d095ab429
SilasEduardo <silasandrade94@gmail.com>
2022-07-19 21:32:40 -0300

    reprise

---
+++ funções-avançadas/aula-11/assets/js/main.js
@@ -0,0 +1,58 @@
+function CreateCalculator() {
+    let display = document.querySelector('.display')
+       this.init=()=>{
+        this.createBtn()
+       }
+        
+
+
+    this.createBtn=()=>{
+        document.addEventListener('click', (e)=>{
+            let el = e.target
+            if(el.classList.contains('btn-num')){
+                this.displayBtn(el.innerText)
+            };
+            if(el.classList.contains('btn-clear')){
+                this.clearBtn();
+            };
+
+            if(el.classList.contains('btn-del')){
+                this.dellBtn();
+            }
+
+
+            if(el.classList.contains('btn-eq')){
+                this.calculate();
+            }
+
+           
+        })
+    }
+    this.displayBtn=(value)=>{
+        display.value += value;
+    }
+    this.clearBtn=()=>{
+        display.value = '';
+    }
+    this.dellBtn=()=>{
+        display.value = display.value.slice(0, -1);
+    };
+
+    this.calculate=()=>{
+        let calculate = eval(display.value)
+        try{
+            if(!calculate){
+                alert('Conta Invalida!');
+                return
+            }
+            display.value = calculate
+
+        }catch(e){
+            alert('Conta Invalida!', e);
+        }
+    }
+}
+const calculator = new CreateCalculator()
+
+calculator.init()
+
