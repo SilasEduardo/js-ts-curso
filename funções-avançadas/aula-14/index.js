f3d7b816817272c033864bd2c6731f8b679b03a8
SilasEduardo <silasandrade94@gmail.com>
2022-07-22 13:06:31 -0300

    aula 14

---
+++ funções-avançadas/aula-14/index.js
@@ -0,0 +1,38 @@
+// function* geradora() {
+//     yield 'Valor 1';
+
+//     yield 'Valor 2';
+
+//     yield 'Valor 3';
+// }
+
+// const g1 = geradora();
+
+// function* geradora2(){
+//     let i = 0
+//     while(true){
+//        yield i;
+//        i++; 
+
+//     }
+    
+// }
+
+function* geradora3(){
+    yield 0;
+    yield 1;
+    yield 2;
+}
+
+
+function* geradora4(){
+    yield* geradora3()
+    yield 4;
+    yield 5;
+    yield 6;
+}
+
+const g4 = geradora4();
+for(let i of g4){
+    console.log(i)
+}
